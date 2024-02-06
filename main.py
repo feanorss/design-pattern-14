e86754251fd65e1f05bd1ab5a27350a34ab7ee1f
feanorss <madro.milos@gmail.com>
2024-02-06 18:49:05 +0100

    hodina 1. singleton jedna trieda ma jeden objekt

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+class Singleton:
+    _instance = None
+    def __init__(self):
+        if Singleton._instance is not None:
+            raise Exception("Uz je")
+        else:
+            self.meno = "Patrik"
+            Singleton._instance = self
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    @staticmethod
+    def getInstance():
+        if Singleton._instance is None:
+            Singleton()
+        return Singleton._instance
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+s1 = Singleton.getInstance()
+print(s1.meno)
+s2 = Singleton.getInstance()
+s1.meno = "Milan"
+print(s2.meno)
+s3 = Singleton()
