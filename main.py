cc191c3d3d3a8c967566f49a8717449bbf152a50
feanorss <madro.milos@gmail.com>
2024-02-06 20:57:35 +0100

    hodina 1. singleton jedna trieda ma jeden objekt 2.factorz 3.builder 4. observer

--- main.py
+++ main.py
@@ -94,3 +94,73 @@
 # form = builder.add_name_field().add_adress_field().add_email_field().add_country_field().build()
 #
 # print(form)
+
+# class Napoj:
+#     def cena(self):
+#         raise NotImplementedError
+#
+# class Kava(Napoj):
+#     def cena(self):
+#         return 2
+#
+# class Caj(Napoj):
+#     def cena(self):
+#         return 3
+#
+# class PrisadaDecorator(Napoj):
+#     def __init__(self,napoj):
+#         self._napoj = napoj
+#
+#     def cena(self):
+#         return self._napoj.cena()
+#
+# class Mlieko(PrisadaDecorator):
+#     def cena(self):
+#         return self._napoj.cena() + 5
+#
+# class Cukor(PrisadaDecorator):
+#     def cena(self):
+#         return self._napoj.cena() + 2
+#
+# moja_kava = Kava()
+# moja_kava = Mlieko(moja_kava)
+# moja_kava = Cukor(moja_kava)
+# print(moja_kava.cena())
+#
+# moj_caj = Caj()
+# moj_caj = Cukor(moj_caj)
+# print(moj_caj.cena())
+
+class AkciovaBurza:
+    def __init__(self):
+        self._investori = []
+        self._cena_akcie = None
+
+    def pridaj_investora(self, investor):
+        self._investori.append(investor)
+
+    def odstran_investora(self, investor):
+        self._investori.remove(investor)
+
+    def notifikuj_investorov(self):
+        for investor in self._investori:
+            investor.update(self._cena_akcie)
+
+    def set_cena_akcie(self, cena):
+        self._cena_akcie = cena
+        self.notifikuj_investorov()
+
+class Investor:
+    def update(self, cena):
+        print(f"Aktualizovana cena akcie: {cena}")
+
+burza = AkciovaBurza()
+investor1 = Investor()
+investor2 = Investor()
+investor3 = Investor()
+
+burza.pridaj_investora(investor1)
+burza.pridaj_investora(investor2)
+burza.pridaj_investora(investor3)
+
+burza.set_cena_akcie(100)
